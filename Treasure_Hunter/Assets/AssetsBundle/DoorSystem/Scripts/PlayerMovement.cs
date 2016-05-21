5bb8d5cd0764dd952e0555b002f2e605475f33a4
PLaGInc <vladimir.alt@inbox.ru>
2016-05-21 14:15:11 +0300

    interface + new pers
    :+1:

--- Treasure_Hunter/Assets/AssetsBundle/DoorSystem/Scripts/PlayerMovement.cs
+++ Treasure_Hunter/Assets/AssetsBundle/DoorSystem/Scripts/PlayerMovement.cs
@@ -13,20 +13,21 @@ public class PlayerMovement : MonoBehaviour {
 	public float JumpVelocity = 5;
 	public float GroundHeight = 1.1f;
 	private bool jump;
+    Animator anim;
 
-	void Start() { 
-		GetComponent<Rigidbody>().freezeRotation = true;
+    void Start() {
+        anim = GetComponent<Animator>();
+        GetComponent<Rigidbody>().freezeRotation = true;
 		GetComponent<Rigidbody>().useGravity = false;
 	}
 	
 	void Update() {
 		jump = jump || Input.GetButtonDown("Jump");
-	}
+    }
 	
 	void FixedUpdate() {
-	
-		// Cast a ray towards the ground to see if the Walker is grounded
-		bool grounded = Physics.Raycast(transform.position, Gravity.normalized, GroundHeight);
+        // Cast a ray towards the ground to see if the Walker is grounded
+        bool grounded = Physics.Raycast(transform.position, Gravity.normalized, GroundHeight);
 		
 		// Rotate the body to stay upright
 		Vector3 gravityForward = Vector3.Cross(Gravity, transform.right);
@@ -37,20 +38,20 @@ public class PlayerMovement : MonoBehaviour {
 		Vector3 forward = Vector3.Cross(transform.up, -LookTransform.right).normalized;
 		Vector3 right = Vector3.Cross(transform.up, LookTransform.forward).normalized;
 		Vector3 targetVelocity = (forward * Input.GetAxis("Vertical") + right * Input.GetAxis("Horizontal")) * Velocity;
-		Vector3 localVelocity = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
+        Vector3 localVelocity = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
 		Vector3 velocityChange = transform.InverseTransformDirection(targetVelocity) - localVelocity;
-		
-		// The velocity change is clamped to the control velocity
-		// The vertical component is either removed or set to result in the absolute jump velocity
-		velocityChange = Vector3.ClampMagnitude(velocityChange, grounded ? GroundControl : AirControl);
+        // The velocity change is clamped to the control velocity
+        // The vertical component is either removed or set to result in the absolute jump velocity
+        velocityChange = Vector3.ClampMagnitude(velocityChange, grounded ? GroundControl : AirControl);
 		velocityChange.y = jump && grounded ? -localVelocity.y + JumpVelocity : 0;
 		velocityChange = transform.TransformDirection(velocityChange);
 		GetComponent<Rigidbody>().AddForce(velocityChange, ForceMode.VelocityChange);
 		
 		// Add gravity
 		GetComponent<Rigidbody>().AddForce(Gravity * GetComponent<Rigidbody>().mass);
-		
-		jump = false;
-	}
+
+        jump = false;
+        anim.SetFloat("velocity", Mathf.Abs(targetVelocity.z));
+    }
 	
 }
